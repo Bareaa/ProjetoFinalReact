203114e5ae185ca89f6bda704c488b31ee5b08a4
Fernando <fernandojr0227@gmail.com>
2022-12-17 20:12:47 -0300

    links

--- src/App.js
+++ src/App.js
@@ -5,6 +5,13 @@ import {
 } from "react-router-dom";
 import Login from "./pages/Login";
 import Menu from "./pages/Menu";
+import Filmes from "./pages/menus/Filmes";
+import Starships from "./pages/menus/Starships";
+import People from "./pages/menus/People";
+import Planets from "./pages/menus/Planets";
+import Species from "./pages/menus/Species";
+import Duvidas from "./pages/menus/Duvidas";
+import Sobre from "./pages/menus/Sobre";
 import './assets/css/estilos.css'
 
 function App() {
@@ -19,6 +26,34 @@ function App() {
     {
       path: "/menu",
       element: login === true ? <Menu setLogin={setLogin}/> : <Login setLogin={setLogin} />,
+    },
+    {
+      path: "/filmes",
+      element: <Filmes setLogin={setLogin}/>,
+    },
+    {
+      path: "/starships",
+      element: <Starships setLogin={setLogin}/> 
+    },
+    {
+      path: "/people",
+      element: <People setLogin={setLogin}/> 
+    },
+    {
+      path: "/species",
+      element: <Species setLogin={setLogin}/> 
+    },
+    {
+      path: "/duvidas",
+      element: <Duvidas setLogin={setLogin}/> 
+    },
+    {
+      path: "/sobre",
+      element: <Sobre setLogin={setLogin}/> 
+    },
+    {
+      path: "/planets",
+      element: <Planets setLogin={setLogin}/> 
     },
 
   ]);
--- src/pages/menus/Filmes.js
+++ src/pages/menus/Filmes.js
@@ -1,7 +1,7 @@
 import { Grid } from '@mui/material'
 import React from 'react'
-import Cabecalho from '../components/Cabecalho'
-import Rodape from '../components/Rodape'
+import Cabecalho from '../../components/Cabecalho'
+import Rodape from '../../components/Rodape'
 
 export default function Menu(props) {
     const logoff = () => {
@@ -16,14 +16,8 @@ export default function Menu(props) {
         <Grid item md={12} xs={12} sm={12}>
 
           <div className='menu' style={{backgroundImage: "URL(https://th.bing.com/th/id/R.cdcc665c9adff4bca095eeda4f468e39?rik=9rgKSjsdlznLOA&riu=http%3a%2f%2fgetwallpapers.com%2fwallpaper%2ffull%2fd%2f1%2f7%2f30099.jpg&ehk=Yu3WIfdKmzrJihcuK65nAn3HOBY55MDMTM2bmrP0dUI%3d&risl=&pid=ImgRaw&r=0)"}}>
-            <div className='itemMenu' style={{color:"yellow"}}> <a href='menu'>I - MENU</a> </div>
-            <div className='itemMenu' style={{color:"yellow"}}> <a href='filmes'>II - FILMES</a> </div>
-            <div className='itemMenu' style={{color:"yellow"}}> <a href='starships'>III - STARSHIPS</a> </div>
-            <div className='itemMenu' style={{color:"yellow"}}> <a href='people'>IV - PEOPLE</a> </div>
-            <div className='itemMenu' style={{color:"yellow"}}> <a href='planets'>V - PLANETS</a> </div>
-            <div className='itemMenu' style={{color:"yellow"}}> <a href='species'>VI - SPECIES</a> </div>
-            <div className='itemMenu' style={{color:"yellow"}}> <a href='duvidas'>VII - DÚVIDAS</a> </div>
-            <div className='itemMenu' style={{color:"yellow"}}> <a href='sobre'>VIII - SOBRE</a> </div>
+            <div className='itemMenu' style={{color:"yellow"}}> <a href='filmes'> TEST </a></div>
+            <div className='itemMenu' style={{color:"yellow"}}> <a href='menu'> TEST </a> </div>
                 
           </div>
           <Grid>
