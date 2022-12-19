50a472cac7882bd605769774ad908bea6e080136
Fernando <fernandojr0227@gmail.com>
2022-12-19 20:37:08 -0300

    sobre

--- src/pages/menus/Sobre.js
+++ src/pages/menus/Sobre.js
@@ -1,38 +1,46 @@
 import React from "react";
-import DefaultPage from "../components/DefaultPage";
-import "../assets/css/sobre.css";
-import diego from "../assets/images/diego.png";
-import linkedin from "../assets/images/linkedin.png";
-import logo from "../assets/images/logo.png";
-import github from "../assets/images/github.png";
+import "../../assets/css/sobre.css";
+import fernando from "../../assets/images/fernando.jpg";
+import guilherme from '../../assets/images/guilherme.jpg';
+import linkedin from "../../assets/images/linkedin.png";
+import github from "../../assets/images/github.png";
+import Cabecalho from '../../components/Cabecalho';
+import Rodape from '../../components/Rodape';
+
 const Sobre = () => {
   return (
-    <div className="sobre-page">
-      <DefaultPage page="SOBRE" />
+    <div className="conteudoAll">
+      <Cabecalho />
       <div className="sobre">
-        <img className="criador" src={diego} alt=""></img>
-        <div className="buttons">
+      <div className="buttons">
           <h3>
-            Projeto Desenvolvido para a Disciplina de Tópicos Especiais em
-            Programação.
+            Site Star Wars <br/>
+            Trabalho final da disciplina de <b>Tópicos Especiais em Programação</b>.
+            <br/> Desenvolvido por:
           </h3>
-          <p>
-            Tecnologias Utilizadas: React, Firebase, RestAPI com Axios, Material
-            UI, Css, Javascript e HTML5.
-          </p>
-          <div className="links">
-            <a href="https://www.linkedin.com/in/diego-trevisan-cc/">
+        </div>
+        <h1>Fernando</h1>
+        <img className="devs" src={fernando}></img>
+        <div className="links">
+            <a href="https://www.linkedin.com/in/fernando-rodrigues-junior-020188204/">
               <img className="button" src={linkedin} alt="" />
             </a>
-            <a href="https://diegotrevisan.com">
-              <img className="button" src={logo} alt="" />
-            </a>
-            <a href="https://github.com/diegotrevisancc">
+            <a href="https://github.com/frodrigues404">
               <img className="button" src={github} alt="" />
             </a>
-          </div>
+        </div>
+        <h1>Guilherme</h1>
+        <img className="devs" src={guilherme}></img>
+        <div className="links">
+            <a href="https://www.linkedin.com/in/guilherme-barea-3891a5185/">
+              <img className="button" src={linkedin} alt="" />
+            </a>
+            <a href="https://github.com/Bareaa">
+              <img className="button" src={github} alt="" />
+            </a>
         </div>
       </div>
+      <Rodape/>
     </div>
   );
 };
